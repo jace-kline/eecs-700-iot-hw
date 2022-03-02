f7b94a122a55bc73550d1ce128a8b15449097b35
jace-kline <jace.a.kline@gmail.com>
2022-03-01 21:40:54 -0600

    Added comments. Updated requirements.txt. Renamed test.py to hw1.py

--- hw1.py
+++ hw1.py
@@ -1,28 +1,21 @@
-# import smbus
-# import time
-
-# i2c_ch = 1
-# i2c_address = 0x48
-# reg_temp = 0x00
-# reg_config = 0x01
-
-# bus = smbus.SMBus(i2c_ch)
-
-# while True:
-#     val = bus.read_i2c_block_data(i2c_address, reg_temp, 2)
-#     print(val)
-#     time.sleep(500)
-
 import sys
 import time
 import adafruit_dht
 import board
 
+# connected to the GPIO 4 pin
 pin = board.D4
+
+# initialize connection
 dht = adafruit_dht.DHT11(pin)
 
 while True:
+    # get temperature, humidity
     temp = dht.temperature
     hum = dht.humidity
+
+    # print temperature, humidity
     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temp, hum))
+
+    # wait 3 seconds before reading again
     time.sleep(3)
