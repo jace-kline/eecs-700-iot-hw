c6710d79398c314f28323acfbd3ff7a14537e2ba
jace-kline <jace.a.kline@gmail.com>
2022-03-02 19:25:32 -0600

    Refactored the hw1.py file. Reproduced repo on another RPi to verify its working

--- hw1.py
+++ hw1.py
@@ -3,19 +3,53 @@ import time
 import adafruit_dht
 import board
 
-# connected to the GPIO 4 pin
-pin = board.D4
+# A wrapper class for reading the DHT11 sensor
+# Tries to catch and handle occasional read errors
+class DHT11Reader:
+    def __init__(self, pin):
+        self.pin = pin
+        self.dht = adafruit_dht.DHT11(pin)
+        self.temperature = 0
+        self.humidity = 0
+        self.errors_in_a_row = 0
+        self.error_threshold = 5
 
-# initialize connection
-dht = adafruit_dht.DHT11(pin)
+    def read(self):
+        try:
+            # try to read temperature, humidity from sensor
+            self.temperature = self.dht.temperature
+            self.humidity = self.dht.humidity
+            self.errors_in_a_row = 0
+        
+        except RuntimeError as e:
+            # sometimes this error occurs, but it shouldn't be fatal
+            # catch and continue execution
+            if self.errors_in_a_row < self.error_threshold:
+                self.errors_in_a_row += 1
+                print(f"Exception handled. Message = '{str(e)}'")
+            # any other errors must be re-raised
+            else:
+                raise
 
-while True:
-    # get temperature, humidity
-    temp = dht.temperature
-    hum = dht.humidity
+        return self.temperature, self.humidity
 
-    # print temperature, humidity
-    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temp, hum))
+def main():
+    # connected to the GPIO 4 pin
+    pin = board.D4
 
-    # wait 3 seconds before reading again
-    time.sleep(3)
+    # initialize reader
+    reader = DHT11Reader(pin)
+
+    while True:
+        # read temp & humidity data
+        temp, hum = reader.read()
+
+        # print temp & humidity
+        print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temp, hum))
+
+        # wait 1 second before reading again
+        time.sleep(1)
+
+# if this script is executed directly (not imported), then run main()
+if __name__ == "__main__":
+    main()
