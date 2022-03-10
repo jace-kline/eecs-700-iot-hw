d3319e58356c8c01bd03b670229b63dc468457df
jace-kline <jace.a.kline@gmail.com>
2022-03-10 13:17:16 -0600

    Started hw2

---
+++ hw2/publish-test.py
@@ -0,0 +1,67 @@
+import sys
+import time
+import adafruit_dht
+import board
+import paho.mqtt.publish as mqtt_publish
+
+# A wrapper class for reading the DHT11 sensor
+# Tries to catch and handle occasional read errors
+class DHT11Reader:
+    def __init__(self, pin):
+        self.pin = pin
+        self.dht = adafruit_dht.DHT11(pin)
+        self.temperature = 0
+        self.humidity = 0
+        self.errors_in_a_row = 0
+        self.error_threshold = 5
+
+    def read(self):
+        try:
+            # try to read temperature, humidity from sensor
+            self.temperature = self.dht.temperature
+            self.humidity = self.dht.humidity
+            self.errors_in_a_row = 0
+        
+        except RuntimeError as e:
+            # sometimes this error occurs, but it shouldn't be fatal
+            # catch and continue execution
+            if self.errors_in_a_row < self.error_threshold:
+                self.errors_in_a_row += 1
+                print(f"Exception handled. Message = '{str(e)}'")
+            # any other errors must be re-raised
+            else:
+                raise
+
+        return self.temperature, self.humidity
+
+def main():
+    # connected to the GPIO 4 pin
+    pin = board.D4
+
+    # initialize reader
+    reader = DHT11Reader(pin)
+
+    # specify MQTT broker IP address
+    brokerIP = "192.168.86.82"
+
+    while True:
+        # read temp & humidity data
+        temp, hum = reader.read()
+
+        # publish temp and humidity status to MQTT broker
+        publications = [
+            ("temp/status", str(temp), 0, False),
+            ("humidity/status", str(hum), 0, False)
+        ]
+        mqtt_publish.multiple(publications, hostname=brokerIP)
+        print("Published data to MQTT broker")
+
+        # print temp & humidity to terminal
+        print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temp, hum))
+
+        # wait 5 seconds before reading again
+        time.sleep(5)
+
+# if this script is executed directly (not imported), then run main()
+if __name__ == "__main__":
+    main()
